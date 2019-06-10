1fb60fc079ecac951f504dd89d6b431378b16e94
Mathias Kok <mathias.kok@gmail.com>
2019-06-10 12:54:19 +0200

    Header stuff.

--- Projects/Web/HttpWebResponse.fs
+++ Projects/Web/HttpWebResponse.fs
@@ -1,6 +1,7 @@
 module Fiour.Web.HttpWebResponse
 open System.Net
 open Fiour.Web.Cookies
+open Fiour.Web.Headers
 
 type private HWR = HttpWebResponse
 
@@ -13,7 +14,8 @@ let hasStatusCode code =
 let contentType (hwr:HWR) = 
   hwr.ContentType
 
-let hasContentType content = contentType >> ((=)content)
+let hasContentType content = 
+  contentType >> ((=)content)
 
 let cookies (hwr:HWR) =
   Cookies(hwr.Cookies)
@@ -26,3 +28,18 @@ let hasCookieWith pred cookie hwr =
 
 let hasCookie cookie = 
   hasCookieWith (fun _ -> true) cookie
+
+let headers (hwr:HWR) = 
+  Headers(hwr.Headers)
+
+let hasHeaderWith pred (header:string) hwr =
+  let hs = headers hwr
+  match hs.TryGetValue(header) with
+  | (false,_) -> false
+  | (true,h) -> pred h
+
+let hasHeaderEq header value =
+  hasHeaderWith ((=)value) header
+
+let hasHeader header = 
+  hasHeaderWith (fun _ -> true) header
